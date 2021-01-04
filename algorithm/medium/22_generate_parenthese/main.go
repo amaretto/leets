6d710325036f816bdb43acd554aecb087ee6386b
amaretto <jabberwock26@gmail.com>
2021-01-05 00:46:43 +0900

    solve No.22

--- algorithm/medium/22_generate_parenthese/main.go
+++ algorithm/medium/22_generate_parenthese/main.go
@@ -1,19 +1,21 @@
 package main
 
-import "math"
-
 func generateParenthesis(n int) []string {
-	var ans []string
-	if n == 1 {
-		ans = append(ans, "()")
-		return ans
+	return search(0, 0, n, 0, "")
+}
+
+func search(l, r, limit, depth int, s string) []string {
+	var result []string
+	if l == limit && r == limit {
+		return append(result, s)
+	}
+	if r+1 <= limit && depth-1 >= 0 {
+		result = append(result, search(l, r+1, limit, depth-1, s+")")...)
 	}
 
-	for i := int64(math.Pow(2, float64(n)*2-2)); i > 0; i-- {
-		if int(i) == n*2-2 {
-
-		}
+	if l+1 <= limit && depth+1 <= limit {
+		result = append(result, search(l+1, r, limit, depth+1, s+"(")...)
 	}
 
-	return nil
+	return result
 }
--- algorithm/medium/22_generate_parenthese/main_test.go
+++ algorithm/medium/22_generate_parenthese/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -9,14 +10,15 @@ var testCases = []struct {
 	Input  int
 	Output []string
 }{
-	{3, []string{"((()))", "(()())", "(())()", "()(())", "()()()"}},
-	{2, []string{"(())", "()()"}},
 	{1, []string{"()"}},
+	{2, []string{"(())", "()()"}},
+	{3, []string{"((()))", "(()())", "(())()", "()(())", "()()()"}},
 }
 
 func TestGenerateParenthesis(t *testing.T) {
 	for _, testCase := range testCases {
-		result := generateParenthesis(testCase.Input)
+		result := (generateParenthesis(testCase.Input))
+		sort.Strings(result)
 		if !reflect.DeepEqual(result, testCase.Output) {
 			t.Errorf("invalid result. testCase:%v, actual:%v", testCase, result)
 		}
