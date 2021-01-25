2f7aba311f6aaf0234fdbd065dc3b21f2f0845ef
amaretto <jabberwock26@gmail.com>
2021-01-25 23:49:49 +0900

    WIP No.30

--- algorithm/hard/30_substring_with_concatenation_of_all_words/main.go
+++ algorithm/hard/30_substring_with_concatenation_of_all_words/main.go
@@ -1,5 +1,40 @@
 package main
 
 func findSubstring(s string, words []string) []int {
-	return []int{0}
+	var result []int
+	if s == "" || words == nil {
+		return result
+	}
+
+	// create map[word]bool
+	wlength := len(words[0])
+	clength := len(words[0]) * len(words)
+
+	for i := 0; i+clength <= len(s); i += wlength {
+		tmp := s[i : i+clength]
+		exists := make(map[string]bool, wlength)
+		for j := 0; j+wlength <= len(tmp); j += wlength {
+			word := tmp[j : j+wlength]
+			if contains(word, words) && !exists[word] {
+				exists[word] = true
+				if len(exists) == len(words) {
+					result = append(result, i)
+					break
+				}
+			} else {
+				break
+			}
+		}
+	}
+
+	return result
+}
+
+func contains(word string, words []string) bool {
+	for _, w := range words {
+		if word == w {
+			return true
+		}
+	}
+	return false
 }
