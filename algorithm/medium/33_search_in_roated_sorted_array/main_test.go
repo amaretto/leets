80ee901ca9d649cfec3e40a5cda41f47b5365b64
amaretto <jabberwock26@gmail.com>
2021-01-28 23:52:52 +0900

    start to solve No.33

---
+++ algorithm/medium/33_search_in_roated_sorted_array/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+var testCases = []struct {
+	nums   []int
+	target int
+	want   int
+}{
+	{[]int{4, 5, 6, 7, 0, 1, 2}, 0, 4},
+	{[]int{4, 5, 6, 7, 0, 1, 2}, 3, -1},
+	{[]int{1}, 0, -1},
+}
+
+func TestSearch(t *testing.T) {
+	for _, testCase := range testCases {
+		result := search(testCase.nums, testCase.target)
+		if result != testCase.want {
+			t.Errorf("invalid result. testCase:%v, actual:%v", testCase, result)
+		}
+	}
+}
