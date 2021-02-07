ba077693353376480254d1834a6a8e42c88be8e7
amaretto <jabberwock26@gmail.com>
2021-02-07 23:56:23 +0900

    WIP No.25

--- algorithm/hard/WIP_25_reverse_nodes_in_k_group/main.go
+++ algorithm/hard/WIP_25_reverse_nodes_in_k_group/main.go
@@ -6,19 +6,35 @@ type ListNode struct {
 }
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	var sorted, tmp, tail *ListNode
 	var count int
-	tmp, tail = head, head
+	tmp := head
+	var ahead, ghead, ptail, pre, next *ListNode
 
-	for tail != nil {
-		count++
-		if count == k {
-			for t := tmp.Next; t != tail; t = t.Next {
-
-			}
-		}
-		tail = tail.Next
+	if head.Next == nil {
+		return head
 	}
 
-	return head
+	for tmp != nil {
+		next = tmp.Next
+		if count%k == 0 { // head of group
+			ghead = tmp
+		} else if count%k == k-1 { // tail of group
+			ghead.Next = tmp.Next
+			if ptail != nil {
+				ptail.Next = tmp
+			}
+			tmp.Next = pre
+			if ahead == nil {
+				ahead = tmp
+			}
+			ptail = ghead
+		} else {
+			tmp.Next = pre
+		}
+		pre = tmp
+		tmp = next
+		count++
+	}
+
+	return ahead
 }
--- algorithm/hard/WIP_25_reverse_nodes_in_k_group/main_test.go
+++ algorithm/hard/WIP_25_reverse_nodes_in_k_group/main_test.go
@@ -1,21 +1,41 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 var testCases = []struct {
-	head      *ListNode
-	k int
-	want     *ListNode
+	head *ListNode
+	k    int
+	want *ListNode
 }{
-	{&ListNode{1,&ListNode{2,&ListNode{3,&ListNode{4, &ListNode{5,nil}}}}}}, 2, &ListNode{2,&ListNode{1,&ListNode{4,&ListNode{3, &ListNode{5,nil}}}}}}},
+	{&ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}, 2, &ListNode{2, &ListNode{1, &ListNode{4, &ListNode{3, &ListNode{5, nil}}}}}},
+	//{&ListNode{1, nil}, 1, &ListNode{1, nil}},
 }
 
-func TestIsMatch(t *testing.T) {
+func TestReverseKGroup(t *testing.T) {
 	for _, testCase := range testCases {
-		result := isMatch(testCase.Input, testCase.Expression)
-		if result != testCase.Output {
+		result := reverseKGroup(testCase.head, testCase.k)
+		fmt.Println("head")
+		printList(testCase.head)
+		fmt.Println("result")
+		printList(result)
+		if reflect.DeepEqual(result, testCase.want) {
 			t.Errorf("invalid result. testCase:%v, actual:%v", testCase, result)
 		}
 	}
+
+}
+
+func printList(ln *ListNode) {
+	for ln != nil {
+		fmt.Println(ln, ln.Val, ln.Next)
+		ln = ln.Next
+	}
+}
+
+func isSame(r, w *ListNode) {
 
 }
